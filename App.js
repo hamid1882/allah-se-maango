2fc87e98df939e38342ef7890cc047d51369e5c4
hamid1882 <hussainhafeez1100@gmail.com>
2023-10-03 11:35:10 +0530

    Feat: added slider

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import Swiper from "react-native-swiper";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <Swiper showsPagination={true}>
-      <View>
-        <Text>Hello world</Text>
+      <View style={styles.container}>
+        <Text>Hello WOrld</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Hello WOrld</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Hello WOrld</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Hello WOrld</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Hello WOrld</Text>
       </View>
     </Swiper>
   );
 }
 
 const styles = StyleSheet.create({
-  slide: {
+  container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
